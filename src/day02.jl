2104c2182133ffd115336a14b7de36fa763e99f5
maxesit <mpolic2000@gmail.com>
2025-12-11 20:28:05 +0100

    feat: solves Day02 Task2

--- src/day02.jl
+++ src/day02.jl
@@ -4,7 +4,8 @@ using AdventOfCode2025
 
 function day02(input::String=readInput(joinpath(@__DIR__,"..","data","Day02","data-task1.txt")))
 	task1 = sum(countIDs.(inputData(input)[:]))
-	[task1]
+	task2 = sum(countIDs2.(inputData(input)[:]))
+	[task1,task2]
 end
 
 function inputData(s::String)
@@ -45,6 +46,62 @@ function countIDs(RangeL,RangeR)
    end
    counter
 end
+
+countIDs2(v::Vector{Int64}) = countIDs2(v[1],v[2])
+function countIDs2(RangeL,RangeR)
+	counter = 0
+	for num in RangeL:RangeR
+		nstr = string(num)
+		n_digs = length(nstr)
+		flag = false
+		for len in 1:div(n_digs,2)
+			if mod(n_digs,len)==0
+				flag = true
+			    for i in 1:len
+          	    	flag *= all(isequal(first(nstr[i:len:end])),nstr[i:len:end])
+		        	#println("num: ",nstr,":",nstr[i:len:end],"; ",flag)
+				end
+			end
+			if flag==true
+				#println("num: ",nstr,"| with len: ",len)
+				break
+			end
+		end
+		if flag == true
+			counter += num
+		end
+	end
+	counter 
+end
+
+
+
+#=	Idea for part 2:
+
+1. loop from boundary to another
+
+2. Check each number for repetition
+
+=#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 end

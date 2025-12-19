6ae195365950e0f458a2237b97cf471407ac712e
maxesit <mpolic2000@gmail.com>
2025-12-19 19:56:44 +0100

    feat: add template for src

---
+++ src/template.jl
@@ -0,0 +1,11 @@
+module Day##
+
+using AdventOfCode2025
+
+function day##(input::String=readInput(joinpath(@__DIR__,"..","data","Day##","data-task1.txt")))
+	task1 = -1
+	task2 = -1
+	[task1,task2]
+end
+
+end

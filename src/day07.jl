30fb389a05f57c31b8630ec11810ed65775ab909
maxesit <mpolic2000@gmail.com>
2025-12-22 19:10:11 +0100

    feat: solves Day07 Task 2

--- src/day07.jl
+++ src/day07.jl
@@ -3,18 +3,17 @@ module Day07
 using AdventOfCode2025
 
 function day07(input::String=readInput(joinpath(@__DIR__,"..","data","Day07","data-task1.txt")))
-	task1 = countJunctions(transformInput(input))
-	task2 = -1
-	[task1,task2]
+	task1, task2 = countJunctions(transformInput(input))
+	[Int(task1), Int(task2)]
 end
 
 function translate(c)
 	if c == '.'
 		return 0
 	elseif c == 'S'
-		return -1
+		return -5
 	elseif c == '^'
-		return 4
+		return -1
 	elseif c == '|'
 		return 1
 	else
@@ -23,14 +22,15 @@ function translate(c)
 end
 
 # Takes input string -> Outputs the respective matrix 
+## Matrix rules : 
+# -5 for S
+# 0 for .
+# -1 for ^
+# 1 for |
+# +n for n-timelines of |
 function transformInput(s)
 	temp = split(s)
-	# Matrix rules : 
-	# -1 for S
-	# 0 for .
-	# 4 for ^
-	# 1 for | 
-	out = zeros(size(temp,1),length(temp[1]))
+ 	out = zeros(size(temp,1),length(temp[1]))
 	for i in 1:size(out,1)
 		out[i,:] = map(x->translate(x), collect(temp[i]))
 	end
@@ -41,21 +41,21 @@ function countJunctions(manifold)
 	counter = 0
 	temp = copy(manifold)
 	n_layers, d_layers = size(manifold)
-	temp[2,findfirst(x->x==-1.0,manifold[1,:])] = 1
+	temp[2,findfirst(x->x==-5.0,manifold[1,:])] = 1
 	for i in 2:n_layers
 		for j in 1:d_layers
-			if temp[i,j] == 0 # '.'
-				temp[i,j] = (temp[i-1,j] == 1) ? 1 : 0
-			elseif temp[i,j] == 4 # '^'
-				if temp[i-1,j] == 1
-					temp[i,j-1] = 1
-					temp[i,j+1] = 1
+			if temp[i,j] >= 0 # '.' and '|'
+				temp[i,j] += (temp[i-1,j] > 0) ? temp[i-1,j] : 0
+			elseif temp[i,j] == -1 # '^'
+				if temp[i-1,j] > 0 
+					temp[i,j-1] += temp[i-1,j]
+					temp[i,j+1] += temp[i-1,j]
 					counter+=1
 				end
 			end
 		end
 	end
-	counter
+	[counter, sum(temp[end,:])]
 end
 
 end
